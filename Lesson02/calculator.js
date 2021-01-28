fbb882cc4b6a73334f757fe5a21a46239e624c18
hamediaj <hamediajemal2@gmail.com>
2021-01-27 23:30:39 -0800

    completed calculator.js assignment

---
+++ Lesson02/calculator.js
@@ -0,0 +1,33 @@
+/* Write a program called calculator.js. We've demonstrated using CodePen. Feel free to use this tool or another that you are comfortable with.
+
+The calculator will need 4 variables; use all the operators and console log the result of using the two variables. Mix and match the variables in the operators.
+
+Save your work to the Lesson02 directory.
+
+Make a pull request to the parent directory
+*/
+
+//variables
+
+const a = 17;
+const b = 2;
+const c = 5;
+const d = 9;
+
+let addition = a + b;
+console.log(addition);
+
+let subtraction = d - c;
+console.log(subtraction);
+
+let multiplication = b * c;
+console.log(multiplication);
+
+let division = b / d;
+console.log(division);
+
+let modulo = a % d;
+console.log(modulo);
+
+let exponentiation = c ** b;
+console.log(exponentiation);
