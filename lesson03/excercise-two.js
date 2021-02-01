c640032f2ddb52c269672936d29373e572f29912
hamediaj <hamediajemal2@gmail.com>
2021-01-31 23:19:28 -0800

    completed lesson 3 excercise two

---
+++ lesson03/excercise-two.js
@@ -0,0 +1,14 @@
+// Create a JavaScript file that does the following:
+
+// Set a string variable to be a sentence of at least five words and a period.
+// Make an array from that sentence and console.log the third word.
+
+let arr = ['I', 'enjoy', 'baking', 'in', 'my', 'sparetime.'];
+
+
+let str = arr.join(' ');
+console.log(str);
+
+
+let thirdWord = arr[2];
+console.log(thirdWord);
