575f2da2de8259548fce52ad4df1be1f6186c48f
hamediaj <hamediajemal2@gmail.com>
2021-02-03 23:52:49 -0800

    added lesson 3 assignment 3

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,10 @@
+// Create an array that has at least 5 strings:
+
+// Use a for loop to console log all of the elements plus the index and a comma
+
+let arr = ['croissants', 'danishes', 'macarons', 'pies', 'cupcakes'];
+
+for (let i = 0; i < arr.length; i++) {
+
+    console.log(arr[i]);
+}
