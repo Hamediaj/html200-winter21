f797ae3e6d7bcc949b5e6255e885b706e6324864
hamediaj <hamediajemal2@gmail.com>
2021-02-10 15:21:20 -0800

    added lesson4 assignment1

---
+++ Lesson04/excercise-one.js
@@ -0,0 +1,17 @@
+// Write a simple webpage opens an alert that takes a number from the user and squares it and opens an alert with the result. Create an HTML page and write your javascript in a script tag, or create a JavaScript file and link to it from the HTML page.
+
+// You must use a function and an event handler.
+
+// console.log('hi class');
+//
+// let mathProblem = prompt("Enter a numer");
+//
+// console.log(mathProblem);
+//
+// alert('Your new number is' + ' ' + mathProblem*2);
+
+function mathProblem(x) {
+  total = x * x;
+  alert(newNumber);
+}
+mathProblem(x);
